866fa21c93dc76c96654a5c6fb8e98a55f32528c
Joan Ortega <jomaora@gmail.com>
2014-12-07 18:28:25 +0100

    (tp1): Adding routes to handle get, update and delete by id.

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,4 +26,47 @@ router.route('/')
     })
 ;
 
+router.route('/:id')
+    
+    .get(function (req, res) {
+        if (req.params.id >= reviews.length) {
+            res.status(404).send({'error': 'Id of review not found'});
+        } else {
+            res.send(reviews[req.params.id]);    
+        }
+    })
+
+    .delete(function (req, res) {
+        if (!req.params.id) {
+            res.status(403).send({'error': 'Operation Impossible'});
+        } else {
+            if (req.params.id > reviews.length) {
+                res.status(404).send({'error': 'Id of review not found'});
+            } else {
+                delete reviews[req.params.id];
+                res.status(204).send({'message': 'Deleted'});
+            }
+        }
+    })
+
+    .put(function (req, res) {
+        if (!req.params.id) {
+            res.status(400).send({'error': 'Parametres manquants'});
+        } else {
+            if (req.params.id > reviews.length) {
+                res.status(404).send({'error': 'Id of review not found'});
+            } else {
+                var review = reviews[req.params.id];
+
+                review.name = (req.body.name) ? req.body.name : review.name;
+                review.placeType = (req.body.placeType) ? req.body.placeType : review.placeType;
+                review.stars = (req.body.stars) ? req.body.stars : review.stars;
+                
+                reviews[req.params.id] = review;
+                res.status(200).send(reviews[req.params.id]);
+            }
+        }
+    })
+;
+
 module.exports = router;
